82761eb44d024a310254eba7e78ad99350ce582f
Anamikeya <anuraglittleboy78@gmail.com>
2021-07-09 08:32:41 +0530

    second day

---
+++ FirstDay.java
@@ -0,0 +1,20 @@
+package sudhigna;
+
+public class FirstDay {
+	public int a = 10, z = 20, m;
+	boolean isvalid = true;
+	float b = 1.004f;
+	double c = 100000.0001;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub\
+		FirstDay obj = new FirstDay();
+		System.out.println(obj.b);
+		System.out.println(obj.a);
+		System.out.println(obj.c);
+		System.out.println(obj.isvalid);
+		obj.m = obj.a + obj.z;
+		System.out.println(obj.m);
+		
+	}
+
+}
